f60c87e5b513aada00b95509fd2324613a948f4a
Brett Moore & Kelly de Vries <student@epicodus.com>
2015-03-11 15:03:04 -0700

    rendered our who_wins page, and enabled our forms

--- app/app.php
+++ app/app.php
@@ -13,7 +13,11 @@
                 return $app['twig']->render('game.twig');
 
             });
-
+            $app->post("/who_wins", function() use ($app) {
+            $player1 = new RockPaperScissor($_GET['rock1'], $_GET['paper1'], $_GET['scissors1']);
+            $player2 = new RockPaperScissor($_GET['rock2'], $_GET['paper2'], $_GET['scissors2']);
+                return $app['twig']->render('who_wins.twig');
+            });
 
            return $app;
 
